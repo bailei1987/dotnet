fb1b7363c973cc0c6b73d7507bf58d9a924628e5
杜宇 <dygood@outlook.com>
2020-12-16 12:21:05 +0800

    更新血型和港澳台侨胞字典.

--- src/Commons/BL.StandardDictionary/common/person/DicBloodType.cs
+++ src/Commons/BL.StandardDictionary/common/person/DicBloodType.cs
@@ -9,13 +9,13 @@
         {
             return new[]{
                 new DicItem("0","未知血型"),
-                new DicItem("1","A 血型"),
-                new DicItem("2","B 血型"),
-                new DicItem("3","AB 血型"),
-                new DicItem("4","O 血型"),
-                new DicItem("5","RH 阳性血型"),
-                new DicItem("6","RH 阴性血型"),
-                new DicItem("7","HLA 血型"),
+                new DicItem("1","A血型"),
+                new DicItem("2","B血型"),
+                new DicItem("3","AB血型"),
+                new DicItem("4","O血型"),
+                new DicItem("5","RH阳性血型"),
+                new DicItem("6","RH阴性血型"),
+                new DicItem("7","HLA血型"),
                 new DicItem("8","未定血型"),
                 new DicItem("9","其他血型"),
             };
--- src/Commons/BL.StandardDictionary/common/person/DicGATQ.cs
+++ src/Commons/BL.StandardDictionary/common/person/DicGATQ.cs
@@ -8,7 +8,7 @@
         protected override DicItem[] AllOption()
         {
             return new[]{
-                new DicItem("0","非港奥台侨"),
+                new DicItem("0","非港澳台侨"),
                 new DicItem("1","香港同胞"),
                 new DicItem("2","澳门同胞"),
                 new DicItem("3","台湾同胞"),

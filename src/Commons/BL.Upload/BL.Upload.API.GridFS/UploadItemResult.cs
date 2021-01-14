df0ecce256ce1e70484c3b8c91f981621a4486d4
gdj <315297830@qq.com>
2021-01-14 17:25:56 +0800

    no message

---
+++ src/Commons/BL.Upload/BL.Upload.API.GridFS/UploadItemResult.cs
@@ -0,0 +1,10 @@
+﻿namespace BL.Upload.API.GridFS
+{
+    public class UploadItemResult
+    {
+        public string FileId { get; set; }
+        public string FileName { get; set; }
+        public long Length { get; set; }
+        public string ContentType { get; set; }
+    }
+}

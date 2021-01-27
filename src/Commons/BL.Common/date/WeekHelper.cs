622bc631b2eb9394ec1ddbb818634ae6ed576ae6
杜宇 <dygood@outlook.com>
2021-01-27 10:17:29 +0800

    更新扩展函数.

--- src/Commons/BL.Common/date/WeekHelper.cs
+++ src/Commons/BL.Common/date/WeekHelper.cs
@@ -33,7 +33,7 @@ namespace BL.Common.Date
         /// <param name="day"></param>
         /// <param name="type"> 1 ? "周" : "星期"</param>
         /// <returns>周(一至日(天))||星期(一至日(天))</returns>
-        public static string GetDayName(DayOfWeek day, int type = 1)
+        public static string GetDayName(this DayOfWeek day, int type = 1)
         {
             string pre = type == 1 ? "周" : "星期";
             string name = day switch
@@ -49,5 +49,21 @@ namespace BL.Common.Date
             };
             return pre + name;
         }
+
+        /// <summary>
+        /// 获取整周的星期数字形式
+        /// </summary>
+        /// <param name="day"></param>
+        /// <returns></returns>
+        public static int GetDayNumber(this DayOfWeek day) => day switch
+        {
+            DayOfWeek.Friday => 5,
+            DayOfWeek.Monday => 1,
+            DayOfWeek.Saturday => 6,
+            DayOfWeek.Thursday => 4,
+            DayOfWeek.Tuesday => 2,
+            DayOfWeek.Wednesday => 3,
+            _ => 7,
+        };
     }
 }

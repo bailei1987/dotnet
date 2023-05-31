60b422f19275db9efea53dd84035386d83a03312
gdj <315297830@qq.com>
2023-06-01 00:43:15 +0800

    update BL.MongoDBs to use MongoDB.Driver 2.19.2

---
+++ src/Commons/BL.DataSync/BL.DataSync.Server/FieldValueItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BL.DataSync.Server
+{
+    /// <summary>
+    /// 字段值变更条目
+    /// </summary>
+    public class FieldValueItem
+    {
+        public string Ds { get; set; }
+        public string Field { get; set; }
+        public string Value { get; set; }
+        public object MyProperty { get; set; }
+    }
+}
---
+++ src/Commons/BL.DataSync/BL.DataSync.Server/FieldsPoolItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace BL.DataSync.Server
+{
+    /// <summary>
+    /// 字段池设置.每个字段均为唯一注册
+    /// </summary>
+    public class FieldsPoolItem
+    {
+        public FieldsPoolItem()
+        {
+        }
+        /// <summary>
+        /// 数据集(学生,教师,etc)
+        /// </summary>
+        public string Ds { get; set; }
+        /// <summary>
+        /// 字段/属性(Info_Name,Info_Sex,etc)
+        /// </summary>
+        public string Field { get; set; }
+        /// <summary>
+        /// 来源,每个字段的值只能由一个来源变更(通常为API),如学生姓名,仅由学生API更新
+        /// </summary>
+        public string Origin { get; set; }
+    }
+    /// <summary>
+    /// 字段变更跟踪者(字段变更后,向对方发送最新值)
+    /// </summary>
+    public class FieldTracker
+    {
+        public string Tracker { get; set; }
+
+    }
+}
